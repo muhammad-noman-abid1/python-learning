1ec456ae5dbf3cf300a7c5355648c609cf786814
Muhammad Noman Abid <nomanakhonzada2006@gmail.com>
2026-08-21 15:25:21 +0500

    Delete 05_with.py

--- 05_with.py
+++
@@ -1,9 +0,0 @@
-f=open("file.txt")
-print(f.read())
-f.close() 
-
-# The same code can be written Using with statement like this:
-with open("file.txt")as f:
-    print(f.read())
-
-# So now you don't have explicitly close the file by using f.close
